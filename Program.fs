3885be33a750dcf69cd1b6441461cd2d36c99678
YuriyG <yuragonchar93@gmail.com>
2021-04-03 01:09:53 +0300

    Create Expression type. Make validation function with dummy pattern matching

--- Program.fs
+++ Program.fs
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+
 module ArithmeticExpression =  
 
     type Result<'T> =
@@ -10,9 +11,6 @@ module ArithmeticExpression =
     type Parenthes =
     | Open
     | Close
-    type OperatorKind =
-    |Unary
-    |Binary
 
     type Operator = char
     type Number = char
@@ -23,61 +21,88 @@ module ArithmeticExpression =
     | Dot
     | Parenthes of Parenthes
 
+    type ValueExpr = 
+        { Values:LiteralExpression list }
+    type OperationExpr = 
+        { Operation:Operator 
+          Priority:int }
+    type Expression = 
+        | ValueExp of ValueExpr 
+        | OperationExp of OperationExpr
+
     let unfoldResult (res:Result<'T>) =
         match res with
         | Value s -> Some s
         | _ -> None
 
-    let (|Number|_|) (input:char) =
+    let (|NumberPat|_|) (input:char) =
         match input |> Char.IsDigit with
         | true -> Some(input)
         | _ -> None
 
-    let (|Operator|_|) (input:char) =
+    let (|OperatorPat|_|) (input:char) =
         match input with
-        | '+' | '-' | '*' | '/' -> Operator input |> Some
+        | '+' | '-' | '*' | '/' -> OperatorPat input |> Some
         | _ -> None
 
-    let (|Parenthes|_|) = function
+    let (|ParenthesPat|_|) = function
         | '(' -> Open |> Some
         | ')' -> Close |> Some
         | _ -> None
         
-    let matchExpression = function
-        | Number n -> n |> Number |> Value
-        | Operator op -> op |> Operator |> Value
-        | Parenthes p -> p |> Parenthes |> Value
+    let matchLiterals = function
+        | NumberPat n -> n |> Number |> Value
+        | OperatorPat op -> op |> Operator |> Value
+        | ParenthesPat p -> p |> Parenthes |> Value
         | '.' -> Dot |> Value        
         | sym -> Error(sprintf "Invalid symbol %A" sym)
 
-    let parseLiterals (input:string): Result<LiteralExpression list> =    
+    let parseLiterals (input:string): Result<LiteralExpression seq> =    
         if String.IsNullOrEmpty(input) then Error("Expression cannot be empty")
         else
             let expressions = 
                 input 
-                |> Seq.toList 
-                |> (fun lst -> lst @ [' '])
-                |> List.map matchExpression
+                |> Seq.map matchLiterals
             let errors = 
                 expressions                
-                |> List.choose(fun res ->
-                    match res with
+                |> Seq.choose(function
                     |Error err -> Some err
                     | _ -> None)                
-            if errors |> List.isEmpty then 
+            if errors |> Seq.isEmpty then 
                 expressions 
-                |> List.choose unfoldResult
+                |> Seq.choose unfoldResult
                 |> Value
             else
                 errors
                 |> Seq.fold (sprintf "%s\n%s") String.Empty
                 |> Error
 
+    let rec setPriorities (literals:LiteralExpression list) (expressions:Expression list) (level:int): unit = 
+        let matchVals = function
+            |Number n, ValueExp vex -> ()
+            |Number n, OperationExp oex -> ()
+            |Operator o, ValueExp vex -> ()
+            |Operator o, OperationExp oex -> ()
+            |Dot, ValueExp vex -> ()
+            |Dot, OperationExp vex -> ()
+            |Parenthes p, ValueExp vex -> ()
+            |Parenthes p, OperationExp oex -> ()
+        ()
+
+
 open ArithmeticExpression
 [<EntryPoint>]
 let main _ =
     Console.WriteLine("Input arithmetic expression")
-    match Console.ReadLine() |> parseLiterals  with
-    | Value e -> e |> Seq.iter(fun exp -> Console.WriteLine(exp))
+    let getInitialValues (literals:LiteralExpression list):(LiteralExpression list * Expression list) = 
+        match literals with
+        | Operator o :: rest when o = '-' -> (rest, [OperationExp { Operation = '-'; Priority = 0}])
+        | all -> (all, [OperationExp { Operation = '+'; Priority = 0}])
+    
+    match Console.ReadLine() |> parseLiterals with
+    | Value e -> 
+        let literals, expressions = e |> Seq.toList |> getInitialValues
+        setPriorities literals expressions 0 |> ignore
+        ()
     | Error err -> Console.WriteLine(err)
     0 // return an integer exit code
